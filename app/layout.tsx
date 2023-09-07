4f62b29bf5be3d6812e0be64154f87aa9c438b06
younusraza909 <younusraza909@gmail.com>
2023-09-07 02:44:37 -0700

    Theme Toggling between dark and light

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -18,8 +20,17 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          storageKey="discord-theme"
+        >
+          <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+            {children}
+          </body>
+        </ThemeProvider>
       </html>
     </ClerkProvider>
   );
