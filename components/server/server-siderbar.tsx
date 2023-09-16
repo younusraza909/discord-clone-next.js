a4254ad3495684350bf239d9be9d3c952dbfe729
younusraza909 <younusraza909@gmail.com>
2023-09-15 22:40:44 -0700

    server sidebar header

--- components/server/server-siderbar.tsx
+++ components/server/server-siderbar.tsx
@@ -2,6 +2,7 @@ import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { ChannelType } from '@prisma/client';
 import { redirect } from 'next/navigation';
+import ServerHeader from './server-header';
 
 interface ServerSidebarProp {
   serverId: string;
@@ -56,7 +57,11 @@ async function ServerSidebar({ serverId }: ServerSidebarProp) {
     (member) => member.profileId === profile.id
   )?.role;
 
-  return <div>ServerSidebar</div>;
+  return (
+    <div className='flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]'>
+      <ServerHeader server={server} role={role} />
+    </div>
+  );
 }
 
 export default ServerSidebar;
