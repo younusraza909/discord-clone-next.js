13ba979d6d1fc141aebcfc7cfc732ede3712153b
younusraza909 <younusraza909@gmail.com>
2023-09-18 19:20:34 -0700

    Leave Server Integration

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -95,7 +95,9 @@ function ServerHeader({ server, role }: ServerHeaderProps) {
         )}
         {!isAdmin && (
           <DropdownMenuItem
-            onClick={() => {}}
+            onClick={() => {
+              onOpen('leaveServer', { server });
+            }}
             className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
           >
             Leave Server
