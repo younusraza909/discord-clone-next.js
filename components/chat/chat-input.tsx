1435ac97c394e3a6bb8d91f8bf9492554901e823
younusraza909 <younusraza909@gmail.com>
2023-09-25 12:48:43 +0500

    Sending attatchment as channel message

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -7,6 +7,8 @@ import qs from 'query-string';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Plus, Smile } from 'lucide-react';
 
+import { useModal } from '@/hooks/use-modal-store';
+
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { useRouter } from 'next/navigation';
@@ -23,6 +25,7 @@ const formSchema = z.object({
 });
 
 function ChatInput({ apiUrl, name, query, type }: ChatInputProps) {
+  const { onOpen } = useModal();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -60,7 +63,7 @@ function ChatInput({ apiUrl, name, query, type }: ChatInputProps) {
                 <div className='relative p-4 pb-6'>
                   <button
                     type='button'
-                    onClick={() => {}}
+                    onClick={() => onOpen('messageFile', { apiUrl, query })}
                     className='absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center'
                   >
                     <Plus className='text-white dark:text-[#313338]' />
